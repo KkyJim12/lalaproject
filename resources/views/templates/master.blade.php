532c9bf1ef9bfecbcea0c93ecea5b0d6d0df266b
KkyJim12 <jirakarnjim1@gmail.com>
2018-08-28 01:55:10 +0700

    fix asset 2

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -5,17 +5,17 @@
     <meta name="google-signin-client_id" content="330857847010-j3q0mn7ksis1ck3uc5scuf51ci2cpf9e.apps.googleusercontent.com">
     <title></title>
     <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
-    <link rel="stylesheet" href="asset('/assets/css/bootstrap.min.css')">
-    <link rel="stylesheet" href="asset('/assets/css/bootstrap-social.css')">
-    <link rel="stylesheet" href="asset('/assets/css/custom.css')">
-    <link rel="stylesheet" href="asset('/assets/css/dropzone.min.css')">
-    <link href="asset('/assets/css/hover.css')" rel="stylesheet" media="all">
+    <link rel="stylesheet" href=asset('/assets/css/bootstrap.min.css')>
+    <link rel="stylesheet" href=asset('/assets/css/bootstrap-social.css')>
+    <link rel="stylesheet" href=asset('/assets/css/custom.css')>
+    <link rel="stylesheet" href=asset('/assets/css/dropzone.min.css')>
+    <link href=asset('/assets/css/hover.css') rel="stylesheet" media="all">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-    <script src="asset('/assets/js/jquery-3.1.1.min.js')" type="text/javascript"></script>
-    <script src="asset('/assets/js/jquery.loading-indicator.min.js')"></script>
-    <script src="asset('/assets/js/bootstrap.min.js')" type="text/javascript"></script>
-    <script src="asset('/assets/js/custom.js')" type="text/javascript"></script>
-    <script src="asset('/assets/js/dropzone.js')"></script>
+    <script src=asset('/assets/js/jquery-3.1.1.min.js') type="text/javascript"></script>
+    <script src=asset('/assets/js/jquery.loading-indicator.min.js')></script>
+    <script src=asset('/assets/js/bootstrap.min.js') type="text/javascript"></script>
+    <script src=asset('/assets/js/custom.js') type="text/javascript"></script>
+    <script src=asset('/assets/js/dropzone.js')></script>
 
   </head>
   <body>

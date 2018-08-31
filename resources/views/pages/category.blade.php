f40cdb93673ae5b9e56b7b2532eeb9db58e00577
KkyJim12 <jirakarnjim1@gmail.com>
2018-08-31 23:19:06 +0700

    str 42

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -14,7 +14,7 @@
       <div class="card hvr-grow-shadow" style="width:100%;">
         <img class="card-img-top course-img" src="/assets/img/course/{{$course_in_categorys->course_img}}" alt="course_img">
         <div class="card-body">
-          <h4 class="card-title course-title">{{str_limit($course_in_categorys->course_name,30)}}</h4><hr>
+          <h4 class="card-title course-title">{{str_limit($course_in_categorys->course_name,42)}}</h4><hr>
           <p>{{$course_in_categorys->course_teacher_name}}</p>
           <h2 class="card-text"><span class="badge badge-primary">฿ {{$course_in_categorys->course_price}}</span><span class="badge badge-info" style="float:right; margin-top:5px;">{{$course_in_categorys->course_now_joining}}/{{$course_in_categorys->course_max}}</span></h2>
           <small class="text-muted">เริ่มเรียน {{date('d/m/Y', strtotime($course_in_categorys->course_start_date))}} ถึง {{date('d/m/Y', strtotime($course_in_categorys->course_end_date))}}</small>
